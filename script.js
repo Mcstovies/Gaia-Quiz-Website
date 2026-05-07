63a23622c442000938b46627f0261062fad57893
Adam Clarke <adamclarke0905@gmail.com>
2026-05-07 18:51:23 +0100

    declared variables of the questions and answers

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 // ===============================================================
 // DECLARING VARIABLES AND ARRAYS OF QUESTIONS, OPTIONS, AND ANSWERS
 // ===============================================================
+
+const questions = [
+    "What gas do trees absorb?",
+    "What should you recycle?",
+    "What causes global warming?",
+    "Which energy is renewable?",
+    "What happens when ice caps melt?",
+    "What helps reduce pollution?",
+];
+
+const options = [
+  ["Oxygen", "Carbon Dioxide", "Nitrogen"],
+  ["Plastic", "Glass", "Both"],
+  ["Cars", "Trees", "Rain"],
+  ["Solar", "Coal", "Oil"],
+  ["Sea levels rise", "Nothing", "Mountains grow"],
+  ["Cycling", "Driving more", "Burning fuel"]
+];
+
+const answers = [2,3,1,1,1,1];
+
+let score = 0;
+let currentQuestion = 0;
+
+// ===============================================================
+// HTML ELEMENTS
+// ===============================================================
+
+
